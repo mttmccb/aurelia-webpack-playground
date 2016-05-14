b6cde6e105e563a9a3e2f727714ec1666b9b785e
Matt McCabe <mattmadethis2@gmail.com>
2016-05-14 23:06:36 +0100

    Removed config loader, no longer needed

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -42,7 +42,6 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel', exclude: /node_modules/, query: { presets: ['es2015-loose', 'stage-1'], plugins: ['transform-decorators-legacy'] } },
       { test: /\.css?$/, loader: 'style!css' },
-      { test: /\.config$/, loader: 'file' },
       { test: /\.html$/, loader: 'html' },
       { test: /\.(png|gif|jpg)$/, loader: 'url?limit=8192' },
       { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff2' },
