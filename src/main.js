2488a219fb6ce20824ab6ef4ccf0330d25dc0093
Matt McCabe <mattmadethis2@gmail.com>
2016-05-16 22:46:35 +0100

    another test...

--- src/main.js
+++ src/main.js
@@ -14,23 +14,21 @@ Promise.config({
 
 import { bootstrap } from 'aurelia-bootstrapper-webpack';
 
-import 'bootstrap';
-
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/font-awesome/css/font-awesome.css';
+import 'bootstrap-webpack';
+import 'font-awesome-webpack';
 import '../styles/styles.css';
 
 bootstrap(function(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
+    .plugin('aurelia-validatejs')
     .plugin('aurelia-dialog', config => {
       config.useDefaults();
       config.settings.lock = true;
       config.settings.centerHorizontalOnly = false;
       config.settings.startingZIndex = 5;
-    })
-    .plugin('aurelia-validatejs');
+    });
 
   aurelia.start().then(() => aurelia.setRoot('app', document.body));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,8 @@ module.exports = {
   plugins: [
     new AureliaWebpackPlugin({
       includeSubModules: [
-        { moduleId: 'aurelia-validatejs' },
-        { moduleId: 'aurelia-dialog' }
+        { moduleId: 'aurelia-dialog' },
+        { moduleId: 'aurelia-validatejs' }
       ]
     }),
     new ProvidePlugin({
@@ -40,9 +40,10 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/, query: { presets: ['es2015-loose', 'stage-1'], plugins: ['transform-decorators-legacy'] } },
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules|local_packages/, query: { presets: ['es2015-loose', 'stage-1'], plugins: ['transform-decorators-legacy'] } },
       { test: /aurelia-dialog\/dist\/commonjs\/dialog\.css/, loader: 'raw'},
       { test: /\.css$/, exclude: /aurelia-dialog\/dist\/commonjs\/dialog\.css/, loader: 'style!css' },
+      { test: /\.less$/, loader: "style!css!less"},
       { test: /\.html$/, loader: 'html' },
       { test: /\.(png|gif|jpg)$/, loader: 'url?limit=8192' },
       { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff2' },
